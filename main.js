17016c6d4fecc22d6f3e825c52db1becd7b2f19b
Denis Klyuev <denis.klyuev@gmail.com>
2024-07-18 01:55:43 -0400

    set up basic THREE scene

--- main.js
+++ main.js
@@ -1,58 +1,29 @@
-import './style.css'
+import './style.css';
 
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+import * as THREE from 'three';
 
 const width = 1200;
 const height = 900;
+const canvas = document.querySelector('canvas');
 canvas.width = width;
 canvas.height = height;
 
-const data = [
-    {label: 'JavaScript', value: 20, color: '#f3f057'},
-    {label: 'TypeScript', value: 40, color: '#245eaa'},
-    {label: 'Node.js', value: 40, color: '#68a559'},
-    {label: 'C#', value: 20, color: '#255117'},
-    {label: 'Java', value: 10, color: '#713725'},
-];
-const total = data.reduce((acc, item) => acc + item.value, 0);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
 
-let currentAngle = 0.5;
-const legendX = width * 0.75;
-let legendY = 50;
-const textSize = width * 0.02;
-const lineHeight = textSize * 1.4;
-ctx.globalCompositeOperation = 'source-over';
-ctx.fillStyle = 'white';
-ctx.fillRect(0, 0, width, height);
-ctx.fillStyle = 'black';
-ctx.fillRect(legendX - 4, legendY - 4, width / 6, data.length * lineHeight);
-const pieRadius = height * 0.3;
-for (const {label, value, color} of data) {
-    const angle = (value / total) * Math.PI * 2;
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-    ctx.save();
+camera.position.z = 5;
 
-    // pie
-    ctx.translate(width / 2, height / 2);
-    ctx.rotate(currentAngle);
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.arc(0, 0, pieRadius, 0, angle);
-    ctx.fill();
-    ctx.restore();
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+});
+renderer.setSize( width, height );
 
-    // legend color
-    ctx.fillStyle = color;
-    ctx.fillRect(legendX, legendY, lineHeight / 1.6, lineHeight / 1.6);
-
-    // legend label
-    ctx.fillStyle = 'white';
-    ctx.textBaseline = 'top';
-    ctx.font = `normal ${textSize}px sans-serif`;
-    ctx.fillText(label, legendX + lineHeight * 0.8, legendY);
-
-    legendY += lineHeight;
-    currentAngle += angle;
+function animate() {
+    renderer.render( scene, camera );
 }
+renderer.setAnimationLoop( animate );
